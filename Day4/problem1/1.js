540727f648143b9878937bce2ce74230d1f09ae9
Prari2725 <pragatirpehrkar@gmail.com>
2026-02-23 11:04:19 +0530

    added day 4 content

---
+++ Day4/problem1/1.js
@@ -0,0 +1,4 @@
+
+let a=[2,2,3,4];
+let r=a.reduce((b,c)=>b*c)
+console.log(r);
---
+++ Day4/problem6/num.js
@@ -0,0 +1,15 @@
+
+let num = 7;  
+
+let result = (num >= 0) ? "Positive" : "Negative";
+console.log("Number is:", result);
+
+if (num % 2 === 0) {
+    console.log("Even Number");
+} else {
+    console.log("Odd Number");
+}
+
+for (let i = 1; i <= num; i++) {
+    console.log(i);
+}
