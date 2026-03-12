65689c2e6bedd5fddc022b9a590ae20dff2171ee
Prari2725 <pragatirpehrkar@gmail.com>
2026-03-12 18:05:24 +0530

    Added Day 18 Content

---
+++ Day18/EncapsulationEg/EncapsulationEg/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class BankAccount
+{
+    private double bal;
+
+    public BankAccount(double initialBalance)
+    {
+        bal = initialBalance;
+    }
+
+    public void Deposit(double amt)
+    {
+        bal += amt;
+        Console.WriteLine("Deposit successful: " + amt);
+    }
+
+    public void Withdraw(double amt)
+    {
+        if (amt > bal)
+        {
+            Console.WriteLine("Insufficient balance");
+        }
+        else
+        {
+            bal -= amt;
+            Console.WriteLine("Withdraw successful: " + amt);
+        }
+    }
+
+    public double GetBalance()
+    {
+        return bal;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount acc = new BankAccount(2000);
+
+        int choice;
+        double amount;
+
+        Console.WriteLine("1. Deposit");
+        Console.WriteLine("2. Withdraw");
+        Console.WriteLine("3. Check Balance");
+
+        Console.Write("Enter your choice: ");
+        choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.Write("Enter amount to deposit: ");
+                amount = Convert.ToDouble(Console.ReadLine());
+                acc.Deposit(amount);
+                break;
+
+            case 2:
+                Console.Write("Enter amount to withdraw: ");
+                amount = Convert.ToDouble(Console.ReadLine());
+                acc.Withdraw(amount);
+                break;
+
+            case 3:
+                Console.WriteLine("Current Balance = " + acc.GetBalance());
+                break;
+
+            default:
+                Console.WriteLine("Invalid Choice");
+                break;
+        }
+
+        Console.WriteLine("Final Balance = " + acc.GetBalance());
+    }
+}
---
+++ Day19/collection1/collection1/Program.cs
@@ -0,0 +1,95 @@
+﻿//using System.Collections;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+
+
+//        ArrayList arr = new ArrayList();
+//        arr.Add(10);
+//        arr.Add(200);
+//        arr.Add(400);
+//        arr.Add(200);
+//        arr.Add("Revature");
+//        arr.RemoveAt(3);
+//        arr.RemoveRange(1, 2);
+//        arr.Insert(1, 40);
+
+//        foreach (object i in arr)
+//        {
+//            Console.WriteLine(i);
+
+
+//        }
+
+//    }
+//    }
+
+
+//using System.Collections;
+
+//class HashTable
+//{
+//        static void Main(string[] args)
+//{
+//    Hashtable ht = new Hashtable();
+//    ht.Add("eid", 1001);
+//    ht.Add("eid1", 1002);
+//    ht.Add("name", "amit");
+//    ht.Add("Job", "Developer");
+//    ht.Add("Salary", 50000);
+//    ht.Add("location", "delhi");
+//    foreach (object obj in ht.Keys)
+//    {
+//        Console.WriteLine(obj + " :" + ht[obj]);
+//    }
+//    Console.WriteLine("Location" + " :" + ht["location"]);
+
+
+//    //Console.WriteLine("is eid key exist or not");
+//    // ht.ContainsKey("eid");
+//    Console.WriteLine("is eid key exist or not" + ht.ContainsKey("r"));
+//}
+//    }
+
+
+
+
+
+using System.Collections;
+
+class Hash1
+{
+
+    static void Main(string[] args)
+    {
+        Stack st = new Stack();
+        st.Push("deepti");
+        st.Push("Amit");
+        st.Push("ankit");
+        Console.WriteLine(st.Count);
+        Console.WriteLine(st.Peek());//display top element without removing it
+        Console.WriteLine(st.Count);
+        st.Pop();
+        Console.WriteLine(st.Count);
+        st.Pop();
+        Console.WriteLine(st.Count);
+        Console.WriteLine(st.Contains("Amit"));
+        st.Push("Amit");
+        st.Push("ankit");
+
+
+
+
+        foreach (object ob in st)
+        {
+            Console.WriteLine(ob);
+        }
+    }
+}
+
+
+
+
+
